b71fdeee477b86140c562ab62dbc4b4ddb93c13d
John Doe <david.arfi69100@hotmail.fr>
2025-08-28 11:47:48 +0200

    MAJ3

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,11 +1,11 @@
-import { Col, Container, Tab } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
 export const ProjectCard = ({ title, description, imgUrl}) => {
     
     return (
         <Col size={12} sm={6} md={4}>
             <div className="proj-imgbx">
-                <img src={imgUrl} />
+                <img src={imgUrl} alt="projet"/>
                 <div className="proj-txtx">
                     <h4>{title}</h4>
                     <span>{description}</span>
