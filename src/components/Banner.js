479d43e622943c8d4989442ea5f112a418e866cf
John Doe <david.arfi69100@hotmail.fr>
2025-08-28 11:35:08 +0200

    MAJ

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap"
-import { ArrowRightCircle } from "react-bootstrap-icons"
 import headerImg from "../assets/img/header-img.svg"
 import 'animate.css';
 import TrackVisibility from "react-on-screen";
@@ -14,19 +13,11 @@ export const Banner = () => {
     const [delta, setDelta] = useState(300 - Math.random() * 100);
     const period = 2000;
 
-    useEffect(() => {
-        let ticker = setInterval(() => {
-            tick();
-        },delta)
-    
-        return () => { clearInterval(ticker)};
-    }, [text])
-
     const tick = () => {
       let i = loopNum %   toRotate.length;
       let fullText = toRotate[i];
       let updatedText = isDeleting ? fullText.substring(0, text.length -1) : fullText.substring(0, text.length + 1);
-      
+
       setText(updatedText);
 
       if (isDeleting) {
@@ -42,6 +33,14 @@ export const Banner = () => {
         setDelta(500);
       }
     }
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        },delta)
+    
+        return () => { clearInterval(ticker)};
+    }, [text, delta, tick])
 
     return (
         <section className="banner" id="home">
